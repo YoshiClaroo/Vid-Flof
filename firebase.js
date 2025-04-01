6ea7b4cb4afb84f9066ace5001db77003314a297
YoshiClaroo <braianm313@gmail.com>
2025-04-01 18:47:02 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,18 +5,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyANjIecB-LakVfSjbjd-Z3TsDlm0tbXEfU",
   authDomain: "vid-flof.firebaseapp.com",
   projectId: "vid-flof",
-  storageBucket: "vid-flof.firebasestorage.app",
+  storageBucket: "vid-flof.appspot.com",
   messagingSenderId: "842823438154",
-  appId: "1:842823438154:web:0b2a73c5f85e8fe5613bb3",
-  measurementId: "G-6X1PK66KNQ"
+  appId: "1:842823438154:web:0b2a73c5f85e8fe5613bb3"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Funciones para usar en tu proyecto
-window.dbFunctions = {
+export const dbFunctions = {
   saveLink: async (id, data) => {
     await setDoc(doc(db, "links", id), data);
   },
