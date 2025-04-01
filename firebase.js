78f0d06854f13ec532ff037e544cc777f53283d0
YoshiClaroo <braianm313@gmail.com>
2025-04-01 18:29:50 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,25 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC42OFPPU85yaIKF42a1MTUKux5lYrqXVs",
-  authDomain: "video-pop-2ff08.firebaseapp.com",
-  projectId: "video-pop-2ff08",
-  storageBucket: "video-pop-2ff08.firebasestorage.app",
-  messagingSenderId: "1014152964865",
-  appId: "1:1014152964865:web:5c565d26f2aa89642f8900"
+  apiKey: "AIzaSyANjIecB-LakVfSjbjd-Z3TsDlm0tbXEfU",
+  authDomain: "vid-flof.firebaseapp.com",
+  projectId: "vid-flof",
+  storageBucket: "vid-flof.firebasestorage.app",
+  messagingSenderId: "842823438154",
+  appId: "1:842823438154:web:0b2a73c5f85e8fe5613bb3",
+  measurementId: "G-6X1PK66KNQ"
 };
 
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+// Funciones para usar en tu proyecto
 window.dbFunctions = {
-  saveLink: async (id, data) => await setDoc(doc(db, "links", id), data),
+  saveLink: async (id, data) => {
+    await setDoc(doc(db, "links", id), data);
+  },
   getLink: async (id) => {
     const docSnap = await getDoc(doc(db, "links", id));
     return docSnap.exists() ? docSnap.data() : null;
