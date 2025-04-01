11399973d4219e80675fb4678fe22b7d81329402
YoshiClaroo <braianm313@gmail.com>
2025-04-01 19:03:55 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyANjIecB-LakVfSjbjd-Z3TsDlm0tbXEfU",
-  authDomain: "vid-flof.firebaseapp.com",
-  projectId: "vid-flof",
-  storageBucket: "vid-flof.appspot.com",
-  messagingSenderId: "842823438154",
-  appId: "1:842823438154:web:0b2a73c5f85e8fe5613bb3"
+    authDomain: "vid-flof.firebaseapp.com",
+    projectId: "vid-flof",
+    storageBucket: "vid-flof.firebasestorage.app",
+    messagingSenderId: "842823438154",
+    appId: "1:842823438154:web:0b2a73c5f85e8fe5613bb3",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export const dbFunctions = {
-  saveLink: async (id, data) => {
-    await setDoc(doc(db, "links", id), data);
-  },
+window.dbFunctions = {
+  saveLink: async (id, data) => await setDoc(doc(db, "links", id), data),
   getLink: async (id) => {
     const docSnap = await getDoc(doc(db, "links", id));
     return docSnap.exists() ? docSnap.data() : null;
