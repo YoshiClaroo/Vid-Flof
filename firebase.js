03ae0612a108e7df7698d553188789a1c1a7ffeb
YoshiClaroo <braianm313@gmail.com>
2025-04-01 19:14:33 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,18 +3,22 @@ import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/fireb
 
 const firebaseConfig = {
   apiKey: "AIzaSyANjIecB-LakVfSjbjd-Z3TsDlm0tbXEfU",
-    authDomain: "vid-flof.firebaseapp.com",
-    projectId: "vid-flof",
-    storageBucket: "vid-flof.firebasestorage.app",
-    messagingSenderId: "842823438154",
-    appId: "1:842823438154:web:0b2a73c5f85e8fe5613bb3",
+  authDomain: "vid-flof.firebaseapp.com",
+  projectId: "vid-flof",
+  storageBucket: "vid-flof.firebasestorage.app",
+  messagingSenderId: "842823438154",
+  appId: "1:842823438154:web:0b2a73c5f85e8fe5613bb3"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-window.dbFunctions = {
-  saveLink: async (id, data) => await setDoc(doc(db, "links", id), data),
+// 👇 Exporta las funciones correctamente
+export const dbFunctions = {
+  saveLink: async (id, data) => {
+    await setDoc(doc(db, "links", id), data);
+    console.log("Documento guardado con ID:", id);
+  },
   getLink: async (id) => {
     const docSnap = await getDoc(doc(db, "links", id));
     return docSnap.exists() ? docSnap.data() : null;
